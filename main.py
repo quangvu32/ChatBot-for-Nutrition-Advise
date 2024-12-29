2478f95614bfe7335e9023890ecf5264bfc62365
AkhongloiChin <116686227+AkhongloiChin@users.noreply.github.com>
2024-12-29 21:56:23 +0700

    chat_history para not added

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from cross_encoder import reranking
 init_user_db()
 chat_history = []
 
-def router(query: str):
+def router(query: str,chat_history):
     print(">Router")
     route = rl(query)
-    
+    print(chat_history)
     if route.name == 'small_talk':
         print("small talking")
         response = small_talker(query, chat_history)
@@ -26,11 +26,13 @@ def router(query: str):
         print(f"Parse results with right unit : {g_parse_results}")
         for result in g_parse_results:
             food_data = search_nutrition(result.get('food'))
+            print("yes")
             if len(food_data) == 0:
                 return "Sorry I couldn't find your food in our database"
             elif len(food_data) == 1:
                 insert_user_data(food_data,result['amount'])
             else:
+                print("yes yes")
                 ori_name = result.get('food')
                 food_names = [entry[0] for entry in food_data]  
                 final = reranking(ori_name, food_names)
@@ -42,7 +44,6 @@ def router(query: str):
                     if food[0] == final:
                         fin = food
                         print("found it")
-                        break
                 insert_user_data(fin,result['amount'])
         response = "Saving complete"
         data = view_diet()
@@ -74,12 +75,13 @@ for message in chat_history:
 user_input = st.text_input("Ask me anything about nutrition:")
 
 if user_input:
-    response = router(user_input)
+    response = router(user_input,chat_history)
     
     st.markdown(f"**Bot:** {response}")
 
     chat_history.append(HumanMessage(content=user_input))
     chat_history.append(AIMessage(content=response))
+
 
 if st.button("Clear Chat"):
     chat_history.clear()
