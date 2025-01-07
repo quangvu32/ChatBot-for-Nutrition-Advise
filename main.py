beb18ed030d394a3b275ecee1a2440d9c9b72905
AkhongloiChin <116686227+AkhongloiChin@users.noreply.github.com>
2025-01-07 21:22:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from user_data_tool import init_user_db, view_diet, search_nutrition, insert_use
 from query_agent import extract_food_with_llm
 from unitconverter import convert_to_grams
 from cross_encoder import reranking
+import time
+
 init_user_db()
 chat_history = []
 
-def router(query: str,chat_history):
+def router(query: str, chat_history):
     print(">Router")
     route = rl(query)
-    print(chat_history)
+    
     if route.name == 'small_talk':
         print("small talking")
         response = small_talker(query, chat_history)
@@ -26,13 +28,11 @@ def router(query: str,chat_history):
         print(f"Parse results with right unit : {g_parse_results}")
         for result in g_parse_results:
             food_data = search_nutrition(result.get('food'))
-            print("yes")
             if len(food_data) == 0:
                 return "Sorry I couldn't find your food in our database"
             elif len(food_data) == 1:
                 insert_user_data(food_data,result['amount'])
             else:
-                print("yes yes")
                 ori_name = result.get('food')
                 food_names = [entry[0] for entry in food_data]  
                 final = reranking(ori_name, food_names)
@@ -44,6 +44,7 @@ def router(query: str,chat_history):
                     if food[0] == final:
                         fin = food
                         print("found it")
+                        break
                 insert_user_data(fin,result['amount'])
         response = "Saving complete"
         data = view_diet()
@@ -64,24 +65,49 @@ def router(query: str,chat_history):
     chat_history.append(AIMessage(content=response))
     
     return response
+st.sidebar.title("Nutrition Advisor Chatbot")
 
-st.title("Nutrition Advisor Chatbot")
-for message in chat_history:
-    if isinstance(message, HumanMessage):
-        st.markdown(f"**You:** {message.content}")
-    elif isinstance(message, AIMessage):
-        st.markdown(f"**Bot:** {message.content}")
+def history(chat_history):
+    for message in chat_history:
+        if isinstance(message, HumanMessage):
+            with st.chat_message('user'):
+                st.write(message.content)
+        elif isinstance(message, AIMessage):
+            with st.chat_message('assistant'):
+                st.write(message.content)
 
-user_input = st.text_input("Ask me anything about nutrition:")
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = chat_history
+
+history(st.session_state.chat_history)
+
+user_input = st.chat_input("Ask me anything about nutrition:")
 
 if user_input:
-    response = router(user_input,chat_history)
-    
-    st.markdown(f"**Bot:** {response}")
 
+    st.session_state.chat_history.append(HumanMessage(content=user_input))
     chat_history.append(HumanMessage(content=user_input))
+    with st.chat_message('user'):
+        st.write(user_input)
+
+    response = router(user_input,chat_history)
+
+    st.session_state.chat_history.append(AIMessage(content=response))
     chat_history.append(AIMessage(content=response))
+    with st.chat_message('assistant'):
+        full_res = ""
+        holder = st.empty()
 
-
-if st.button("Clear Chat"):
+        for word in response.split():
+            full_res += word + " "
+            time.sleep(0.05)
+            holder.markdown(full_res + "▋")
+        holder.markdown(full_res) 
+        
+    
+# if st.button("Clear Chat"):
+#     st.session_state.chat_history = []
+if st.sidebar.button("Clear Chat"):
     chat_history.clear()
+    st.session_state.chat_history = chat_history
+    history(st.session_state.chat_history)
