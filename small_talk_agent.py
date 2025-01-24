868301437f1b2cbd62230c8d1501ec4fcb629705
AkhongloiChin <116686227+AkhongloiChin@users.noreply.github.com>
2025-01-24 22:10:41 +0700

    Update small_talk_agent.py

--- small_talk_agent.py
+++ small_talk_agent.py
@@ -1,17 +1,21 @@
 from langchain_ollama import OllamaLLM
 from langchain.chains import ConversationChain
 from langchain.schema import HumanMessage, AIMessage
-
+import time
 # Initialize the Ollama LLM
 llm = OllamaLLM(model='llama3')
 
 # Define a conversational chain
 conversation = ConversationChain(llm=llm)
 
-def small_talker(input_text: str, chat_history: str) -> str:
+def small_talker(input_text: str, chat_history) -> str:
+    start_time = time.time()
+    if len(chat_history) > 6: 
+        chat_history = chat_history[-6:]
+    print(chat_history)
     prompt = f"{input_text}\nHere are the conversation history: \n{chat_history}"
     output = conversation.invoke(prompt)
+    print("--- %s small talk agent ---" % (time.time() - start_time))
     if isinstance(output, dict) and "response" in output:
         return output["response"]
     return str(output)
-
