05fbf5aeb1de1a96702ce803842244589b05e604
AkhongloiChin <116686227+AkhongloiChin@users.noreply.github.com>
2025-01-24 22:10:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from query_agent import extract_food_with_llm
 from unitconverter import convert_to_grams
 from cross_encoder import reranking
 import time
+from delete import delete_table
 
 init_user_db()
-chat_history = []
 
 def router(query: str, chat_history):
     print(">Router")
@@ -21,14 +21,16 @@ def router(query: str, chat_history):
         response = small_talker(query, chat_history)
     
     elif route.name == 'diet_tracker':
+        start_time = time.time()
         print("diet tracking")
         parse_results = extract_food_with_llm(query,chat_history) #Parse results: [{'food': 'apples', 'amount': '100 grams'}]
-        print(f"Parse results: {parse_results}")
+        #print(f"Parse results: {parse_results}")
         g_parse_results = convert_to_grams(parse_results) #Parse results with right unit : [{'food': 'apples', 'amount': 100.0}]
-        print(f"Parse results with right unit : {g_parse_results}")
+        #print(f"Parse results with right unit : {g_parse_results}")
         for result in g_parse_results:
             food_data = search_nutrition(result.get('food'))
             if len(food_data) == 0:
+                print("--- %s diet_tracking ---" % (time.time() - start_time))
                 return "Sorry I couldn't find your food in our database"
             elif len(food_data) == 1:
                 insert_user_data(food_data,result['amount'])
@@ -36,17 +38,17 @@ def router(query: str, chat_history):
                 ori_name = result.get('food')
                 food_names = [entry[0] for entry in food_data]  
                 final = reranking(ori_name, food_names)
-                print("ori_names :", ori_name)
-                print("food_names :",food_names)
-                print("final:", final, "type:", type(final))
-                print("food_data:", food_data, "type:", type(food_data))
+                #print("ori_names :", ori_name)
+                #print("food_names :",food_names)
+                #print("final:", final, "type:", type(final))
+                #print("food_data:", food_data, "type:", type(food_data))
                 for food in food_data :
                     if food[0] == final:
                         fin = food
-                        print("found it")
                         break
                 insert_user_data(fin,result['amount'])
         response = "Saving complete"
+        print("--- %s diet_tracking ---" % (time.time() - start_time))
         data = view_diet()
         if data:
             st.write("Here is the list of diet reports:")
@@ -75,6 +77,8 @@ def history(chat_history):
         elif isinstance(message, AIMessage):
             with st.chat_message('assistant'):
                 st.write(message.content)
+                
+chat_history = []
 
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = chat_history
@@ -104,10 +108,9 @@ if user_input:
             holder.markdown(full_res + "▋")
         holder.markdown(full_res) 
         
-    
-# if st.button("Clear Chat"):
-#     st.session_state.chat_history = []
 if st.sidebar.button("Clear Chat"):
     chat_history.clear()
     st.session_state.chat_history = chat_history
     history(st.session_state.chat_history)
+if st.sidebar.button("Delete database"):
+    delete_table()
